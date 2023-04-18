ffa42f7be100c59bf816b94934ee1336b3d02915
lidiagregorio <130047702+lidiagregorio@users.noreply.github.com>
2023-04-18 17:53:10 +0200

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,14 @@
+data = df
+df = pd.DataFrame(data)
+
+llista gran = df.values.totlist ()
+
+canals_i_videos = []
+
+count = 0
+
+for canal in llista_gran [6]:
+
+canals_i_videos = [canal]
+
+print(canals_i_videos)
